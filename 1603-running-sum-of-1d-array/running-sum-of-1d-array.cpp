2dcfdcd0ca92afb2bec16b9ee041107d3464eb89
Priyanka Deshmukh <priyankadeshmukh.h@gmail.com>
2023-08-26 22:12:59 +0530

    Time: 5 ms (28.20%) | Memory: 8.5 MB (16.27%) - LeetSync

---
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> ans;
+        int t =0;
+        for(int i =0;i<nums.size();i++)
+        {
+            t= t + nums[i];
+            ans.push_back(t);
+        }
+        return ans;
+    }
+};
