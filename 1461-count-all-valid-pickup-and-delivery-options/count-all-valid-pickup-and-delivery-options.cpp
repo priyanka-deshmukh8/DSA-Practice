e8c73fd8c1a032d82a977022a160eccc8194a2bd
Priyanka Deshmukh <priyankadeshmukh.h@gmail.com>
2023-09-10 23:40:25 +0530

    Time: 0 ms (100.00%) | Memory: 5.9 MB (96.89%) - LeetSync

--- 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.cpp
+++ 1461-count-all-valid-pickup-and-delivery-options/count-all-valid-pickup-and-delivery-options.cpp
@@ -1,14 +1,17 @@
 class Solution {   
 public:
-const int MOD = 1e9 + 7;
+const int M = 1e9 + 7;
 int countOrders(int n) {
-  long long count=1;
-  
-
-  for(int i=2; i<=n; i++) {
-    count =(count * (2*i-1)*i)% MOD;
+  if(n==1)
+    return 1;
+  long long result = 1;
+  for(int i =2;i<=n;i++)
+  {
+    int space = (i-1)*2+1;
+    int temp = space * (space+1)/2;
+    result *= temp;
+    result%=M;
   }
-
-  return count;
-    }
+  return result;
+  }
 };
